4882d52d8d3386cd5bf93a96e521eff83ebe81d8
ptakondrej <florixakcz@seznam.cz>
2026-06-23 19:19:41 +0200

    feat: create hero cta buttons

---
+++ components/hero/hero-cta.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+const HeroCTA = () => {
+  return (
+    <div className="flex items-center gap-4">
+      <Button asChild>
+        <Link href="#projects">
+          View Projects <ArrowUpRight className="size-4" />
+        </Link>
+      </Button>
+      <Button variant="outline" asChild>
+        <Link href="#contact">
+          Contact Me <ArrowUpRight className="size-4" />
+        </Link>
+      </Button>
+    </div>
+  );
+};
+
+export default HeroCTA;
