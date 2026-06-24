27a45c45bed088e85ff5c40e92e721bea94e9261
ptakondrej <florixakcz@seznam.cz>
2026-06-24 08:49:02 +0200

    feat: display tech stack component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero/hero";
+import TechStack from "@/components/stack/tech-stack";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <TechStack />
     </>
   );
 }
