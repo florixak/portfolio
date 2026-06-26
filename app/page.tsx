86b742fb6bc603e056e8d14799603131f4b9d63f
ptakondrej <florixakcz@seznam.cz>
2026-06-26 08:45:05 +0200

    refactor: update import to ContactFooter

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Contact from "@/components/contact/contact";
+import ContactFooter from "@/components/contact/contact-footer";
 import FeaturedProjects from "@/components/featured/featured-projects";
 import Hero from "@/components/hero/hero";
 import TechStack from "@/components/stack/tech-stack";
@@ -9,7 +9,7 @@ export default function Home() {
       <Hero />
       <TechStack />
       <FeaturedProjects />
-      <Contact />
+      <ContactFooter />
     </>
   );
 }
